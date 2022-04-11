e8bab80eb09c5a13cc6741c4764c1a758a1127c8
Yosuke Watanabe <y.watanabe@amazia.co.jp>
2022-04-11 17:38:35 +0900

    Supported initial parameters for rememberZoomableState

--- zoomable/src/main/java/moe/tlaster/zoomable/ZoomableState.kt
+++ zoomable/src/main/java/moe/tlaster/zoomable/ZoomableState.kt
@@ -28,12 +28,18 @@ import kotlinx.coroutines.launch
 fun rememberZoomableState(
     @FloatRange(from = 0.0) minScale: Float = 1f,
     @FloatRange(from = 0.0) maxScale: Float = Float.MAX_VALUE,
+    @FloatRange(from = 0.0) initialTranslateX: Float = 0f,
+    @FloatRange(from = 0.0) initialTranslateY: Float = 0f,
+    @FloatRange(from = 0.0) initialScale: Float = minScale,
 ): ZoomableState = rememberSaveable(
     saver = ZoomableState.Saver
 ) {
     ZoomableState(
         minScale = minScale,
         maxScale = maxScale,
+        initialTranslateX = initialTranslateX,
+        initialTranslateY = initialTranslateY,
+        initialScale = initialScale
     )
 }
 
